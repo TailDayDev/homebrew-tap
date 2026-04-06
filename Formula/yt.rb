4d370123e55ec3164ff33cb1a91f68b7e350e994
Bogdan Nasikovsky <bogdan808@ya.ru>
2026-04-06 10:17:02 +0300

    Update yt to 0.2.0

--- Formula/yt.rb
+++ Formula/yt.rb
@@ -1,8 +1,8 @@
 class Yt < Formula
   desc "TailDay YouTrack CLI"
   homepage "https://github.com/TailDayDev/yt"
-  url "https://codeload.github.com/TailDayDev/yt/tar.gz/refs/tags/v0.1.0"
-  sha256 "36b4c9731a00045aad4274dee5f73b3ad3fb0d31b03769240d2d8a70f533ff30"
+  url "https://codeload.github.com/TailDayDev/yt/tar.gz/refs/tags/v0.2.0"
+  sha256 "05d655114b5ffe346e94bf7f4dd910c631f9144d5958cdbc4a6cb4b50b320a44"
   license "UNLICENSED"
 
   depends_on "node"
@@ -23,6 +23,6 @@ class Yt < Formula
   end
 
   test do
-    assert_equal "0.1.0", shell_output("#{bin}/yt version").strip
+    assert_equal "0.2.0", shell_output("#{bin}/yt version").strip
   end
 end
